602686c97fb25311f1e98ecb6fb74c1b0aaa65c3
frosibwz <frosibwz@gmail.com>
2023-05-17 19:14:59 +0300

    app.py

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
 @app.route("/")
-
+@app.route("/index1")
 def hello():
-    return "Hello World!"
+    return render_template('index1.html')
 
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/contact")
+def about():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
 app.run(debug=True)
 
 from json import dumps
